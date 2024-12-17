fd0acfd296a9eea0c1ed88e18564e6d388781793
Jacob Maurer <jacob.maurer.24@cnu.edu>
2024-12-17 17:01:35 -0500

    modified MakeDirectory class to include already built logic; moving to testing

--- new_structure.py
+++ new_structure.py
@@ -1,3 +1,5 @@
+import copy
+
 class Command:
     def __init__(self, alias):
         self.alias = alias
@@ -11,9 +13,16 @@ class MakeDirectory(Command):
     def __init__(self, alias, sub_commands):
         super().__init__(alias)
         self.subcommands = sub_commands
-    def interpret(self, command_str, file_struct, current):
+    def update_dir(self, file_dict, keys, new_dir):
+        if len(keys) == 1 or len(file_dict[keys[0]].keys()) == 0:
+            file_dict[keys[0]].update({new_dir: {}})
+            return 0
+        next_set = file_dict[keys[0]]
+        keys.pop(0)
+        return self.update_dir(next_set, keys, new_dir)
+    def interpret(self, command_str, file_struct, key_path):
         new_struct = file_struct
-        new_current = current
+        new_key_path = key_path
         if command_str[0] not in self.alias:
             raise ValueError("Command is not here!")
         for line in command_str[1:]:
@@ -22,16 +31,15 @@ class MakeDirectory(Command):
                     raise TypeError("Command is formatted wrong! " + line + " sub command does not exsist!")
             else:
                 try:
-                    copied_keys = copy.deepcopy(file_path_keys)
-                    if len(file_path_keys) > 0:
-                        update_dir(file_contents, copied_keys, command[1])
+                    copied_keys = copy.deepcopy(key_path)
+                    if len(copied_keys) > 0:
+                        self.update_dir(file_struct, copied_keys, line)
                     else:
-                        file_contents.update({command[1]: {}})
+                        file_struct.update({line: {}})
                 except Exception as err:
                     print(err)
                     print("Invalid command input!")
-            
-        return new_struct, new_current
+        return new_struct, new_key_path
 
 def run_command(commands_list, command_str):
     new_file_struct = {}
