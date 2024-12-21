336f9b496841d8c711e30356ecfc2e36bcb87085
Jacob Maurer <jacob.maurer.24@cnu.edu>
2024-12-20 20:55:30 -0500

    quick structure for the quickstats (not complete) command

--- commands/quick_stats.py
+++ commands/quick_stats.py
@@ -26,3 +26,28 @@ class StatsObject:
             print("Data is not of type float, double or int!")
     def __str__(self):
         return f"Done!"
+
+class QuickStats(Command):
+    def __init__(self, alias, sub_commands):
+        super().__init__(alias)
+        self.subcommands = sub_commands
+    def interpret(self, command_str, file_struct, current):
+        if command_str[0] not in self.alias:
+            raise ValueError("Command is not here!")
+        if len(command_str[1:]) == 0:
+            try:
+                view = self.get_current_view(file_struct, current) if len(current) != 0 else file_struct
+                print(list(view.keys()))
+            except:
+                print("Something went wrong!")
+        for line in command_str[1:]:
+            if line[0] == '-' and len(self.subcommands) != 0:
+                if line not in self.subcommands:
+                    raise TypeError("Command is formatted wrong! " + line + " sub command does not exsist!")
+            try:
+                view = self.get_current_view(file_struct, current) if len(current) != 0 else file_struct
+                print("Inside List Dir!!!")
+                print(list(view.keys()))
+            except:
+                print("Something went wrong!")
+        return file_struct, current
